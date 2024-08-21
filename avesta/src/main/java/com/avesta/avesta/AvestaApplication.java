e097c8e1966b55a667c42bfd54ce0008a8a6642c
Minko82 <obesecoconut@protonmail.com>
2024-08-20 22:05:35 -0600

    spring initializer

---
+++ avesta/src/main/java/com/avesta/avesta/AvestaApplication.java
@@ -0,0 +1,13 @@
+package com.avesta.avesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvestaApplication.class, args);
+	}
+
+}
---
+++ avesta/src/test/java/com/avesta/avesta/AvestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avesta.avesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
